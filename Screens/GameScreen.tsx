39b909952522de5f669b32b990eacbc1088d75d1
ItsqmetEstudiantes <dilan_torres31@hotmail.com>
2024-07-16 08:43:17 -0500

    Añadido musica a la app

--- Screens/GameScreen.tsx
+++ Screens/GameScreen.tsx
@@ -1,10 +1,10 @@
-import { Platform, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import { WebView } from 'react-native-webview';
 import { useFonts } from 'expo-font';
+import { Audio } from 'expo-av';
 
 export default function GameScreen() {
-
   const [loaded, error] = useFonts({
     'BigBlueTerm': require('../assets/fonts/BigBlueTerm437NerdFont-Regular.ttf'),
   });
@@ -12,21 +12,49 @@ export default function GameScreen() {
   if (!loaded && !error) {
     return null;
   }
+
   const localHtmlFile = require('../Game/pacman.html');
+  const [sound, setSound] = useState<Audio.Sound | null>(null); 
+ 
+  async function playSound() { 
+    const { sound } = await Audio.Sound.createAsync( 
+       require('../assets/sounds/pacman.mp3') 
+    ); 
+    setSound(sound); 
+ 
+    await sound.playAsync(); 
+  } 
+ 
+  useEffect(() => { 
+    playSound(); 
+ 
+    return () => { 
+      if (sound) { 
+        sound.unloadAsync(); 
+      } 
+    }; 
+  }, []);
+
   return (
-    <WebView
-    originWhitelist={['*']}
-      source={localHtmlFile}
-      style={styles.pacman}
-    />
-  )
+    <View style={styles.container}>
+      <WebView
+        originWhitelist={['*']}
+        source={localHtmlFile}
+        style={styles.pacman}
+      />
+      <Button title="Play Sound" onPress={playSound} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  pacman:{
+  container: {
+    flex: 1,
+  },
+  pacman: {
     width: "100%",
     height: "100%",
-    flex: 2,
-    backgroundColor: "rgba (0, 55, 27, 0.8)"
-  }
-})
+    flex: 1, 
+    backgroundColor: "rgba(0, 55, 27, 0.8)", 
+  },
+});
