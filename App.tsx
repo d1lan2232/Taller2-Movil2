d1733fa005887e4960b69f5735b2f9ddb9590ae1
Nano <alhernano@gmail.com>
2024-07-17 10:27:18 -0500

    Removed unnecesary imports from app.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import Navegator from './navigators/BottonNavigator';
 
 export default function App() {
